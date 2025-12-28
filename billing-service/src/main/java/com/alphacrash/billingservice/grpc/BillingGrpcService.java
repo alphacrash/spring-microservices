108d8ff5473939a0897c13d2623186d972c38f8d
alphacrash <sudhansu.ace@gmail.com>
2025-12-28 14:35:32 +0530

    Added Billing Service and Protobuf

---
+++ billing-service/src/main/java/com/alphacrash/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,14 @@
+package com.alphacrash.billingservice.grpc;
+
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<billing.BillingResponse> responseObserver) {
+
+    }
+}
